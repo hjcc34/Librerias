61b39b7328e182c5814b367e9a2b5b1a029303d9
Hermes Colmenares <hermesjcc34@gmail.com>
2025-07-04 20:45:24 +0200

    no message

---
+++ AHT20.X/AHT20_CFG.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   AHT20_CFG.h
+ * Author: Familia CS
+ *
+ * Created on 23 de noviembre de 2024, 21:11
+ */
+
+#ifndef AHT20_CFG_H
+#define	AHT20_CFG_H
+
+#include "../../Reloj_V1/RELOJ_LCD_16x2.X/CFG_16F877A.h"
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* AHT20_CFG_H */
+
